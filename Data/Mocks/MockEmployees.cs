76906ef731159305fcc9c97362817f04817e34a3
Ivan <ivan.p.lebedev@ynadex.ru>
2021-04-22 21:29:43 +0400

    using bootstrap

--- Data/Mocks/MockEmployees.cs
+++ Data/Mocks/MockEmployees.cs
@@ -13,6 +13,10 @@ namespace AccountingOfWorkers.Data.Mocks
         {
             new Employee(){Id = 1, FirstName = "1", Name = "Саня", PhoneNumber = "8 800 235 35 35"},
             new Employee(){Id = 2, FirstName = "2", Name = "Саня2", PhoneNumber = "8 800 235 35 35"},
+            new Employee(){Id = 3, FirstName = "2", Name = "Саня4", PhoneNumber = "8 800 235 35 35"},
+            new Employee(){Id = 4, FirstName = "2", Name = "Саня6", PhoneNumber = "8 800 235 35 35"},
+            new Employee(){Id = 5, FirstName = "2", Name = "Саня7", PhoneNumber = "8 800 235 35 35"},
+            new Employee(){Id = 6, FirstName = "2", Name = "Саня8", PhoneNumber = "8 800 235 35 35"},
         };
 
         public IEnumerable<Employee> MyEmployees => _employees;
